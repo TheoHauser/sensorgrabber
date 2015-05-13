ad8380229c1dc6c032adb84cb74b01b4431bcb0a
Sam <tue95606@temple.edu>
2015-05-13 14:26:03 -0400

    return button in ViewRecordingsActivity

--- app/src/main/java/edu/temple/sensorgrabber/ViewRecordingsActivity.java
+++ app/src/main/java/edu/temple/sensorgrabber/ViewRecordingsActivity.java
@@ -1,11 +1,15 @@
 package edu.temple.sensorgrabber;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by sam rizer on 5/13/2015.
@@ -16,7 +20,15 @@ public class ViewRecordingsActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewrecordings);
 
-        //Somewhere in here create the list of files
+        final Button switchact =(Button)findViewById(R.id.buttonReturn);
+        switchact.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         Log.v("ViewRecordingsActivity:", "Started");
     }
@@ -31,10 +43,5 @@ public class ViewRecordingsActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    public void onClickReturn(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
     }
 }
