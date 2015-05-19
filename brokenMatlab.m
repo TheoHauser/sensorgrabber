90de24ef21510babd1d3f9a0675f04a4d960c007
TheoHauser <theo.hauser@temple.edu>
2015-05-19 16:22:08 -0400

    Create brokenMatlab.m
    This matlab script plots the pitch roll and azimuth and time in seconds but the time is broken and I am going to change how seconds are counted

---
+++ brokenMatlab.m
@@ -0,0 +1,40 @@
+filename = '/home/hauser2016/Project/sensorgrabber/pythonCode/data_with_plots/4-15-2015/05-19-2015 14:51:56.csv';
+
+%open file and change date to datenum format
+fid = fopen(filename, 'rt');
+a = textscan(fid, '%s %f %f %f %f %f %f', 'Delimiter', ',','CollectOutput',1);
+fclose(fid);
+
+%dts is number of seconds in a day
+%filename is first date and time
+dts = 24*3600;
+k = max(strfind(filename,'/'));
+datetime = filename(k+1:(end-4));
+
+formatIn = 'mm-dd-yyyy HH:MM:SS';
+d1 = datenum(datetime, formatIn)*dts;
+formatIn = 'mm/dd/yyyy HH:MM:SS';
+time = ((datenum(M(:,1)))*dts)-d1;
+azimuth = M(:,2);
+pitch = M(:,3);
+roll = M(:,4);
+
+%converting from rad to deg
+azi = rad2deg(azimuth);
+pit = rad2deg(pitch);
+rol = rad2deg(roll);
+
+%smoothing
+azis = smooth(azi,0.3,'rloess');
+pits = smooth(pit,0.3,'rloess');
+rols = smooth(rol,0.3,'rloess');
+
+%unfiltered plot
+figure(1);
+plot(time,azi,'-O', time, pit, '-*', time, rol, '-X');
+
+hold on
+
+%smooth plot
+figure(2);
+plot(time,azis,'-O', time, pits, '-*', time, rols, '-X');
