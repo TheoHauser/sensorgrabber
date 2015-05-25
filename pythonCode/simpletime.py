a8ac35d5d5340a6a31badd9fbff8734c570eefbb
Sam <ComradeNapoleon@users.noreply.github.com>
2015-05-25 14:00:09 -0400

    update simpletime.py
    added a while true loop

--- pythonCode/simpletime.py
+++ pythonCode/simpletime.py
@@ -6,7 +6,7 @@ videoname = raw_input("Enter filename (should be typical Android format, 'VID_YY
 datetime_string = videoname[4:8] + "/" + videoname[8:10] + "/" + videoname[10:12] + " " + videoname[13:15] + ":" + videoname[15:17] + ":" + videoname[17:19]
 dts = datetime.datetime.strptime(datetime_string, "%Y/%m/%d %H:%M:%S")
 
-userprompt = raw_input("Enter occurance: ")
-
-dts += datetime.timedelta(seconds=int(userprompt))
-print dts
+while True:
+  userprompt = raw_input("Enter occurance: ")
+  dts += datetime.timedelta(seconds=int(userprompt))
+  print dts
