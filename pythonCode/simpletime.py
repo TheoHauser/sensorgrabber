2ac4ebaddfe92457a94f834c89b539b1c3b86492
Sam <ComradeNapoleon@users.noreply.github.com>
2015-05-26 14:19:18 -0400

    Update simpletime.py

--- pythonCode/simpletime.py
+++ pythonCode/simpletime.py
@@ -1,12 +1,37 @@
 import datetime
+import os
 
-#videoname = "VID_20150519_102443.mp4"
-videoname = raw_input("Enter filename (should be typical Android format, 'VID_YYYYMMDD_HHMMSS.mp4'): \n")
+path = "/home/reu/HealthAppVideos/"
 
-datetime_string = videoname[4:8] + "/" + videoname[8:10] + "/" + videoname[10:12] + " " + videoname[13:15] + ":" + videoname[15:17] + ":" + videoname[17:19]
-dts = datetime.datetime.strptime(datetime_string, "%Y/%m/%d %H:%M:%S")
+def read_files(path):
+  files = []
+  for name in os.listdir(path)
+    if os.path.isfile(os.path.join(path, name)):
+      files.append(name)
+  return files
 
+def choose_file(uFiles):
+  print("Options: ")
+  filecount = 0
+  for filecount in range(0, len(uFiles)):
+    print(str(filecount) + ". " + str(uFiles[filecount])
+  index = input("Enter choice number: ")
+  return uFiles[index]
+
+def file_to_datetime(filelist):
+  datetime_string = (videoname[4:8] + "/" + videoname[8:10] + "/" + videoname[10:12] + " "
+                  + videoname[13:15] + ":" + videoname[15:17] + ":" + videoname[17:19])
+  return datetime.datetime.strptime(datetime_string, "%Y/%m%d %H:%M:%S")
+
+filelist = read_files(path)
 while True:
-  userprompt = raw_input("Enter occurance: ")
-  dts += datetime.timedelta(seconds=int(userprompt))
-  print dts
+  videoname = choose_file(filelist)
+  dts = file_to_datetime(videoname)
+  loop = True
+  while loop:
+    userprompt = raw_input("Enter occurance or 'Q' to quit: ")
+    if (userprompt == 'q' or userprompt == 'Q'):
+      loop = False
+    else:
+      returntime = dts + datetime.timedelta(seconds=int(userprompt))
+    print returntime
