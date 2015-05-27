5c8950149ad870d46aa805aa37e75e054ca6085b
Sam <ComradeNapoleon@users.noreply.github.com>
2015-05-27 10:30:00 -0400

    Update timestamper.py
    Loads all the videos from one file and sends it to another folder devoted to "timestamped" videos

--- pythonCode/timestamper.py
+++ pythonCode/timestamper.py
@@ -3,27 +3,37 @@ import time
 from PIL import Image, ImageDraw, ImageFont
 import moviepy.editor as mp
 from moviepy.video.io.bindings import PIL_to_npimage
+import os
 
-#TODO: Ability to specify path name and file name
-
-path = "/home/reu/HealthAppVideos/"
-videoname = "VID_20150519_102443.mp4"
-
-datetime_string = videoname[4:8] + "/" + videoname[8:10] + "/" + videoname[10:12] + " " + videoname[13:15] + ":" + videoname[15:17] + ":" + videoname[17:19]
-dts = datetime.datetime.strptime(datetime_string, "%Y/%m/%d %H:%M:%S")
-runtime = time.time()
-nowtime = 0
+IN_PATH = "/home/reu/HealthAppVideos/"
+OUT_PATH = "/home/reu/HealthAppVideos/Timestamped/"
 
 fontname = "/usr/share/fonts/truetype/freefont/FreeMono.ttf"
 font = ImageFont.FreeTypeFont(fontname, 36)
 
+def load_folder(path):
+	files = []
+	for name in os.listdir(path):
+		if os.path.isfile(os.path.join(path, name)):
+			files.append(name)
+	return files
+
+def make_datetime_string(videoname):
+	#Manually-defined substrings are okay because it's the same for all videos recorded on android
+	datetime_string = (videoname[4:8] + "/" + videoname[8:10] + "/" + videoname[10:12] + " "
+			+ videoname[13:15] + ":" + videoname[15:17] + ":" + videoname[17:19])
+	dts = datetime.datetime.strptime(datetime_string, "%Y/%m/%d %H:%M:%S")
+	return dts 
+
 def timestamp_filter(video_frame, t):
-	printtime = dts + datetime.timedelta(seconds=t)
+	printtime = make_datetime_string(folder[i]) + datetime.timedelta(seconds=t)
 	im = Image.fromarray(video_frame(t))
 	draw = ImageDraw.Draw(im)
 	draw.text((2, 2), str(printtime), font=font)
 	return PIL_to_npimage(im)
 
-clip = mp.VideoFileClip(path+videoname)
-timestamped_clip =clip.fl(timestamp_filter)
-timestamped_clip.write_videofile(path + "TIMESTAMPED_" + videoname, bitrate='8000k')
+folder = load_folder(IN_PATH)
+for i in range(0, len(folder)):
+	clip = mp.VideoFileClip(IN_PATH+folder[i])
+	timestamped_clip = clip.fl(timestamp_filter)
+	timestamped_clip.write_videofile(OUT_PATH + "TIMESTAMPED_" + folder[i], bitrate='8000k')
