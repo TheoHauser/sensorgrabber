13eeab33c083d16f2e2723709f8992c585a0732c
Sam <ComradeNapoleon@users.noreply.github.com>
2015-05-27 09:33:04 -0400

    brokentime.py became timestamp.py
    now it works

--- pythonCode/timestamper.py
+++ pythonCode/timestamper.py
@@ -5,7 +5,6 @@ import moviepy.editor as mp
 from moviepy.video.io.bindings import PIL_to_npimage
 
 #TODO: Ability to specify path name and file name
-#TODO: Get the time to update per frame
 
 path = "/home/reu/HealthAppVideos/"
 videoname = "VID_20150519_102443.mp4"
@@ -13,20 +12,18 @@ videoname = "VID_20150519_102443.mp4"
 datetime_string = videoname[4:8] + "/" + videoname[8:10] + "/" + videoname[10:12] + " " + videoname[13:15] + ":" + videoname[15:17] + ":" + videoname[17:19]
 dts = datetime.datetime.strptime(datetime_string, "%Y/%m/%d %H:%M:%S")
 runtime = time.time()
+nowtime = 0
 
 fontname = "/usr/share/fonts/truetype/freefont/FreeMono.ttf"
 font = ImageFont.FreeTypeFont(fontname, 36)
 
-def timestamp_filter(video_frame):
-        nowtime = time.time()
-        nowtime -= runtime
-        dts += datetime.timedelta(seconds=nowtime)
-        im = Image.fromarray(video_frame)
-        draw = ImageDraw.Draw(im)
-        draw.text((2, 2), str(dts), font=font)
-        return PIL_to_npimage(im)
+def timestamp_filter(video_frame, t):
+	printtime = dts + datetime.timedelta(seconds=t)
+	im = Image.fromarray(video_frame(t))
+	draw = ImageDraw.Draw(im)
+	draw.text((2, 2), str(printtime), font=font)
+	return PIL_to_npimage(im)
 
 clip = mp.VideoFileClip(path+videoname)
 timestamped_clip =clip.fl(timestamp_filter)
-#dts += datetime.timedelta(seconds=1)
-timestamped_clip.write_videofile(path + "TIMESTAMPED" + videoname, bitrate='8000k')
+timestamped_clip.write_videofile(path + "TIMESTAMPED_" + videoname, bitrate='8000k')
