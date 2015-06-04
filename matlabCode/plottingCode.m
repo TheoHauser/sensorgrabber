ffa4cb8dab3c3040a225644ab08cb43b83c1365a
TheoHauser <theo.hauser@temple.edu>
2015-06-04 14:51:56 -0400

    change plottingCode back

--- matlabCode/plottingCode.m
+++ matlabCode/plottingCode.m
@@ -1,19 +1,6 @@
 filename = '/home/hauser2016/Project/sensorgrabber/pythonCode/data_with_plots/4-15-2015/05-25-2015 13:37:10.csv';
-%open file and change date to datenum format
-fid = fopen(filename, 'rt');
-a = textscan(fid, '%s %f %f %f %f %f %f', 'Delimiter', ',','CollectOutput',1);
-fclose(fid);
-
-%dts is number of seconds in a day
-%filename is first date and time
-dts = 24*3600;
-k = max(strfind(filename,'/'));
-datetime = filename(k+1:(end-4));
-
-formatIn = 'mm-dd-yyyy HH:MM:SS';
-d1 = datenum(datetime, formatIn)*dts;
-formatIn = 'mm/dd/yyyy HH:MM:SS';
-time = ((datenum(M(:,1)))*dts)-d1;
+M = csvread(filename);
+time = M(:,1);
 azimuth = M(:,2);
 pitch = M(:,3);
 roll = M(:,4);
@@ -39,4 +26,3 @@ hold on
 figure(2);
 plot(time,azis,'-O');
 axis([0 inf -180 180]);
-
